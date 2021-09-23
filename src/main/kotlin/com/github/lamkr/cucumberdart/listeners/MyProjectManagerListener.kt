d8fe729f3589d20caab2e28ec5622a78dba56eb4
GitHub Action <action@github.com>
2021-09-23 18:29:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/lamkr/cucumberdart/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/lamkr/cucumberdart/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lamkr.cucumberdart.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.lamkr.cucumberdart.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
